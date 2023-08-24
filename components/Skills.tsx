534ecd24df0715f401beba9af2c8d8230e3173b9
Phillip Kim <phillipkim156@gmail.com>
2023-08-24 15:15:24 +1200

    skills page

---
+++ components/Skills.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import Image from '../node_modules/next/image'
+
+function Skills() {
+  return (
+    <div className='w-full lg:h-screen p-2'>
+        <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+            <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Skills</p>
+            <h2>What I can do</h2>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/html5.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>HTML</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/CSS.png'} alt="/" width="110" height="110"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>CSS</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/typescript.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>TypeScript</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/Nodejs.png'} alt="/" width="150" height="1"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Node.js</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/angular.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Angular</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/React.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>React</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/azuredevops.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Azure DevOps</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/Python.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Python</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/golang.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>GoLang</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={'/assets/swift.png'} alt="/" width="64" height="64"/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Swift</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import About from '../../components/About'
 import Main from '../../components/Main'
 import Navbar from '../../components/Navbar'
+import Skills from '../../components/Skills'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar/>
       <Main/>
       <About/>
+      <Skills/>
     </main>
   )
 }
