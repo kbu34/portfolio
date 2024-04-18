c53ca40dc611633ed5524cefe6db3932b3606196
Phillip Kim <phillipkim156@gmail.com>
2024-04-18 17:25:43 +1200

    Set up template

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,20 @@
 const Projects = () => {
-    return (
-        <div id="projects">
-            hello
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#602aaa]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've built</h2>
+
+        <div className="grid lg:grid-cols-5 gap-8">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full"></div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
