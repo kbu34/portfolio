fae20c4098b007df0eb9b171bfa845a1578f2744
Phillip Kim <phillipkim156@gmail.com>
2024-04-20 17:06:52 +1200

    Replaced data with real data and made changes to CSS

--- components/Project.tsx
+++ components/Project.tsx
@@ -25,9 +25,9 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
     >
       <section
         ref={ref}
-        className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 rounded-lg"
+        className="shadow-lg shadow-gray-400 max-w-[45rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 rounded-lg"
       >
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] group-odd:mr-[18rem]">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pt-5 flex flex-col h-full group-even:ml-[18rem] group-odd:mr-[19rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,17 +1,24 @@
 import image1 from "public/assets/portfolio.png"
 import image2 from "public/assets/bloodstain.png"
+import image3 from "public/assets/wander.png"
 
 export const projectsData = [
     {
-        title: "project one",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        tags: ["one", "two", "three", "four", "five", "six", "seven"],
+        title: "Portfolio Website",
+        description: "The website you are on right now! I made this website to learn Next.js and Tailwind CSS. I also use this website to try things that I think are cool.",
+        tags: ["Next.js", "Tailwind", "TypeScript", "React"],
         imageUrl: image1
     },
     {
-        title: "project two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        tags: ["one", "two", "three", "four", "five", "six", "seven"],
+        title: "Automated Bloodstain Segmentation",
+        description: "Industrial project in conjunction with ESR. It takes one or more images of bloodstains and determines what type of bloodstain they are using convolutional neural network.",
+        tags: ["AI", "Computer Vision", "Deep Learning", "Python"],
         imageUrl: image2
+    },
+    {
+        title: "Wander",
+        description: "Whole year project with 7 other students. Wander is a service where users can organise and participate in fitness events. These activities can also have metadata such as location.",
+        tags: ["Vue", "Java", "Gherkin", "JavaScript", "SQL", "API"],
+        imageUrl: image3
     }
 ] as const
